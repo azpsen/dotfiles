8beb72ddecbfa933ea8cf89559bcb580f4b04989
azpsen <aprl.petersen@gmail.com>
2024-01-31 08:40:17 -0600

    bar & picom fixes, restructuring

--- .config/bspwm/scripts/notify.sh
+++ .config/bspwm/scripts/notify.sh
@@ -5,11 +5,15 @@ while true; do
     # Check if feishin player exists
     player_exists=$(playerctl --player=Feishin status > /dev/null 2>&1; echo $?)
     if (( player_exists != 0 )); then
-        echo "Feishin player not found."
-        sleep 5
+        sleep 1
         continue
     fi
     
+    status=$(playerctl -p Feishin status)
+    if [[ x"$status" == x"Stopped" ]]; then
+        sleep 1
+        continue
+    fi
     current_track=$(playerctl -p Feishin metadata --format '<i>{{ title }}</i>\n{{ artist }}\n{{ album }}')
     album_art_url=$(playerctl -p Feishin metadata mpris:artUrl)
     album_art_file="/tmp/album_art.jpg"
